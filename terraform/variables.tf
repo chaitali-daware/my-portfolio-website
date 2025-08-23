da192383c597c6a5f7311f0af8db40c55bc4bd46
Chaitali Charandas Daware <126700530+chaitali-daware@users.noreply.github.com>
2025-08-23 20:17:36 +0530

    variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,29 +1,6 @@
-variable "aws_region" {
-  default = "ap-south-1"
-}
+variable "project_name" { type = string  default = "portfolio" }
+variable "aws_region"   { type = string  default = "ap-south-1" }
 
-variable "domain_name" {
-  description = "Your custom domain from Namecheap"
-  type        = string
-}
-
-variable "s3_bucket_name" {
-  description = "S3 bucket name for static website"
-  type        = string
-}
-
-variable "lambda_contact_name" {
-  default = "handleContactForm"
-}
-
-variable "lambda_visitor_name" {
-  default = "logVisitorData"
-}
-
-variable "dynamodb_contact_table" {
-  default = "ContactFormSubmissions"
-}
-
-variable "dynamodb_visitor_table" {
-  default = "VisitorLogs"
-}
+# Domain names (Namecheap)
+variable "domain_name"  { type = string  default = "chaitalidaware.me" }
+variable "alt_domain"   { type = string  default = "www.chaitalidaware.me" }
