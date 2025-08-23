829dbc87497a9dfedf134c71482db4c364f2c875
Chaitali Charandas Daware <126700530+chaitali-daware@users.noreply.github.com>
2025-08-23 18:51:32 +0530

    outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,14 +1,18 @@
 output "bucket_name" {
-  value       = aws_s3_bucket.portfolio_bucket.id
-  description = "Name of the S3 bucket"
+  value = aws_s3_bucket.portfolio_bucket.bucket
 }
 
 output "cloudfront_domain" {
-  value       = aws_cloudfront_distribution.portfolio_distribution.domain_name
-  description = "CloudFront distribution domain"
+  value = aws_cloudfront_distribution.portfolio_distribution.domain_name
 }
 
-output "api_gateway_url" {
-  value       = aws_api_gateway_stage.api_stage.invoke_url
-  description = "Invoke URL of the API Gateway"
+output "contact_form_api_url" {
+  value = "${aws_apigatewayv2_api.contact_form_api.api_endpoint}/contact"
 }
+
+output "visitor_api_url" {
+  value = "${aws_apigatewayv2_api.visitor_api.api_endpoint}/visit"
+}
+
+output "acm_validation_records" {
+  value = aws_acm_certificate.cert.domain_validation_options
