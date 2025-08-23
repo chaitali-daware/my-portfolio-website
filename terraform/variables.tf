755b1cc34a4b91c9d2a50d5db34f40c127ef7a27
Chaitali Charandas Daware <126700530+chaitali-daware@users.noreply.github.com>
2025-08-23 18:47:55 +0530

    variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,31 +1,21 @@
+variable "bucket_name" {
+  description = "S3 bucket name for the portfolio"
+  type        = string
+}
+
+variable "domain_name" {
+  description = "Custom domain name for the portfolio"
+  type        = string
+}
+
 variable "region" {
-  description = "AWS region"
+  description = "AWS region for resources"
+  type        = string
+  default     = "ap-south-1"
+}
+
+variable "us_east_region" {
+  description = "AWS region for ACM certificate (must be us-east-1)"
   type        = string
   default     = "us-east-1"
-}
-
-variable "bucket_name" {
-  description = "Name of the S3 bucket for hosting"
-  type        = string
-}
-
-variable "acm_certificate_arn" {
-  description = "ARN of the ACM certificate for CloudFront"
-  type        = string
-}
-
-variable "mime_types" {
-  description = "Mapping of file extensions to MIME types"
-  type        = map(string)
-  default = {
-    ".html" = "text/html"
-    ".css"  = "text/css"
-    ".js"   = "application/javascript"
-    ".png"  = "image/png"
-    ".jpg"  = "image/jpeg"
-    ".jpeg" = "image/jpeg"
-    ".gif"  = "image/gif"
-    ".svg"  = "image/svg+xml"
-    ".ico"  = "image/x-icon"
-  }
 }
