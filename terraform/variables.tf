ea279dcf327cf0320069a9d8f43bcf12d79c06a6
Chaitali Charandas Daware <126700530+chaitali-daware@users.noreply.github.com>
2025-08-23 18:31:11 +0530

    variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -7,3 +7,25 @@ variable "domain_name" {
   description = "Custom domain name for the portfolio"
   type        = string
 }
+
+variable "mime_types" {
+  description = "Mapping of file extensions to MIME types for S3 uploads"
+  type        = map(string)
+  default = {
+    ".html" = "text/html"
+    ".css"  = "text/css"
+    ".js"   = "application/javascript"
+    ".png"  = "image/png"
+    ".jpg"  = "image/jpeg"
+    ".jpeg" = "image/jpeg"
+    ".gif"  = "image/gif"
+    ".svg"  = "image/svg+xml"
+    ".json" = "application/json"
+  }
+}
+
+variable "region" {
+  description = "AWS region where resources will be created"
+  type        = string
+  default     = "us-east-1"
+}
