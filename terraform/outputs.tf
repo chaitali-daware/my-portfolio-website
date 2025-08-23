c1fadd30ddc138d19ac0b4fd3f057a86149f030e
Chaitali Charandas Daware <126700530+chaitali-daware@users.noreply.github.com>
2025-08-23 18:31:40 +0530

    outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,12 +1,19 @@
 output "bucket_name" {
-  value = aws_s3_bucket.portfolio_bucket.bucket
+  description = "The name of the S3 bucket hosting the portfolio website"
+  value       = aws_s3_bucket.portfolio_bucket.bucket
 }
 
 output "cloudfront_domain" {
-  value = aws_cloudfront_distribution.portfolio_distribution.domain_name
+  description = "The domain name of the CloudFront distribution"
+  value       = aws_cloudfront_distribution.portfolio_distribution.domain_name
 }
 
-output "acm_domain_validation" {
-  description = "Add this DNS record in Namecheap for SSL validation"
-  value       = aws_acm_certificate.cert.domain_validation_options
+output "certificate_arn" {
+  description = "The ARN of the ACM certificate"
+  value       = aws_acm_certificate.cert.arn
+}
+
+output "api_gateway_url" {
+  description = "The invoke URL for the API Gateway"
+  value       = aws_api_gateway_rest_api.api.execution_arn
 }
