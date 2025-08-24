3fee67d2d84b9ffa9e959eecfd286e9426779938
Chaitali Charandas Daware <126700530+chaitali-daware@users.noreply.github.com>
2025-08-24 10:04:06 +0530

    main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,18 +2,11 @@ provider "aws" {
   region = var.aws_region
 }
 
-# -------------------------------
-# S3 Bucket
-# -------------------------------
+###########################
+# S3 Bucket for Website
+###########################
 resource "aws_s3_bucket" "website" {
   bucket = var.bucket_name
-}
-
-resource "aws_s3_bucket_website_configuration" "website_config" {
-  bucket = aws_s3_bucket.website.id
-
-  index_document { suffix = "index.html" }
-  error_document { key = "index.html" }
 }
 
 resource "aws_s3_bucket_public_access_block" "block" {
@@ -29,40 +22,49 @@ resource "aws_s3_bucket_policy" "public_read" {
 
   policy = jsonencode({
     Version = "2012-10-17"
-    Statement = [{
-      Effect    = "Allow"
-      Principal = "*"
-      Action    = "s3:GetObject"
-      Resource  = "${aws_s3_bucket.website.arn}/*"
-    }]
+    Statement = [
+      {
+        Effect    = "Allow"
+        Principal = "*"
+        Action    = "s3:GetObject"
+        Resource  = "${aws_s3_bucket.website.arn}/*"
+      }
+    ]
   })
 }
 
-# -------------------------------
-# Upload frontend files
-# -------------------------------
+resource "aws_s3_bucket_website_configuration" "website_config" {
+  bucket = aws_s3_bucket.website.id
+
+  index_document { suffix = "index.html" }
+  error_document { key = "index.html" }
+}
+
+# Upload all frontend files
 resource "aws_s3_object" "website_files" {
-  for_each = { for file in fileset("${path.module}/../frontend", "**/*") : file => file }
+  for_each = { for file in fileset(var.frontend_folder, "**/*") : file => file }
 
   bucket = aws_s3_bucket.website.id
   key    = each.value
-  source = "${path.module}/../frontend/${each.value}"
+  source = "${var.frontend_folder}/${each.value}"
 }
-
-# -------------------------------
-# ACM Certificate
-# -------------------------------
+# ACM certificate (DNS validation)
 resource "aws_acm_certificate" "cert" {
   domain_name       = var.domain_name
   validation_method = "DNS"
 }
 
 resource "aws_route53_record" "acm_validation" {
-  count   = length(aws_acm_certificate.cert.domain_validation_options)
-  name    = aws_acm_certificate.cert.domain_validation_options[count.index].resource_record_name
-  type    = aws_acm_certificate.cert.domain_validation_options[count.index].resource_record_type
-  zone_id = var.route53_zone_id # optional if using Route53
-  records = [aws_acm_certificate.cert.domain_validation_options[count.index].resource_record_value]
+  # This is just for reference if using Route53
+  # For Namecheap, you copy the CNAME manually
+  for_each = {
+    for dvo in aws_acm_certificate.cert.domain_validation_options : dvo.domain_name => dvo
+  }
+
+  zone_id = "" # leave empty, we use Namecheap manually
+  name    = each.value.resource_record_name
+  type    = each.value.resource_record_type
+  records = [each.value.resource_record_value]
   ttl     = 60
 }
 
@@ -70,10 +72,6 @@ resource "aws_acm_certificate_validation" "cert_validation" {
   certificate_arn         = aws_acm_certificate.cert.arn
   validation_record_fqdns = [for record in aws_route53_record.acm_validation : record.fqdn]
 }
-
-# -------------------------------
-# CloudFront Distribution
-# -------------------------------
 resource "aws_cloudfront_distribution" "cdn" {
   origin {
     domain_name = aws_s3_bucket.website.bucket_regional_domain_name
@@ -88,21 +86,78 @@ resource "aws_cloudfront_distribution" "cdn" {
     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
     cached_methods   = ["GET", "HEAD"]
     target_origin_id = "S3-${aws_s3_bucket.website.id}"
-    viewer_protocol_policy = "redirect-to-https"
 
     forwarded_values {
       query_string = false
-      cookies { forward = "none" }
+      cookies {
+        forward = "none"
+      }
+    }
+
+    viewer_protocol_policy = "redirect-to-https"
+  }
+
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
     }
   }
 
   viewer_certificate {
-    acm_certificate_arn      = aws_acm_certificate.cert.arn
-    ssl_support_method       = "sni-only"
-    minimum_protocol_version = "TLSv1.2_2021"
+    acm_certificate_arn            = aws_acm_certificate.cert.arn
+    ssl_support_method             = "sni-only"
+    minimum_protocol_version       = "TLSv1.2_2021"
   }
-
-  restrictions { geo_restriction { restriction_type = "none" } }
 
   aliases = [var.domain_name]
 }
+resource "aws_iam_role" "lambda_role" {
+  name = "lambda_execution_role"
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [{
+      Action    = "sts:AssumeRole"
+      Effect    = "Allow"
+      Principal = { Service = "lambda.amazonaws.com" }
+    }]
+  })
+}
+
+resource "aws_iam_role_policy_attachment" "lambda_policy_attach" {
+  role       = aws_iam_role.lambda_role.name
+  policy_arn = "arn:aws:iam::aws:policy/AWSLambdaBasicExecutionRole"
+}
+
+resource "aws_lambda_function" "handle_contact" {
+  filename         = "${var.lambda_folder}/function.zip"
+  function_name    = "handleContactForm"
+  handler          = "handleContactForm.lambda_handler"
+  runtime          = "python3.11"
+  role             = aws_iam_role.lambda_role.arn
+  source_code_hash = filebase64sha256("${var.lambda_folder}/function.zip")
+}
+
+resource "aws_lambda_function" "log_visitor" {
+  filename         = "${var.lambda_folder}/visitor.zip"
+  function_name    = "logVisitorData"
+  handler          = "logVisitorData.lambda_handler"
+  runtime          = "python3.11"
+  role             = aws_iam_role.lambda_role.arn
+  source_code_hash = filebase64sha256("${var.lambda_folder}/visitor.zip")
+}
+# CloudWatch Alarms for Lambda errors
+resource "aws_cloudwatch_metric_alarm" "lambda_errors" {
+  alarm_name          = "LambdaErrorAlarm"
+  comparison_operator = "GreaterThanThreshold"
+  evaluation_periods  = 1
+  metric_name         = "Errors"
+  namespace           = "AWS/Lambda"
+  period              = 60
+  statistic           = "Sum"
+  threshold           = 0
+
+  dimensions = {
+    FunctionName = aws_lambda_function.handle_contact.function_name
+  }
+}
+
