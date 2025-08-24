7a38d05b54562df8127852dffda3b64716307907
Chaitali Charandas Daware <126700530+chaitali-daware@users.noreply.github.com>
2025-08-24 10:42:31 +0530

    outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -6,6 +6,6 @@ output "s3_website_endpoint" {
   value = aws_s3_bucket.website.bucket_regional_domain_name
 }
 
-output "cloudfront_domain" {
+output "cloudfront_domain_name" {
   value = aws_cloudfront_distribution.cdn.domain_name
 }
