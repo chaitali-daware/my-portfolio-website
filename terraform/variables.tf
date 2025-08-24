50297d70e75d2aefff075c5941e84b17ea5d38a5
Chaitali Charandas Daware <126700530+chaitali-daware@users.noreply.github.com>
2025-08-25 00:15:05 +0530

    variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,26 +1,33 @@
+terraform {
+  required_version = ">= 1.6.0"
+  required_providers {
+    aws     = { source = "hashicorp/aws", version = ">= 5.30" }
+    archive = { source = "hashicorp/archive", version = ">= 2.4.0" }
+  }
+}
+
+provider "aws" {
+  region = var.aws_region
+}
+
 variable "project" {
-  description = "Project name"
-  type        = string
-}
-
-variable "bucket_name" {
-  description = "Name of the S3 bucket"
-  type        = string
-}
-
-variable "domain_name" {
-  description = "Custom domain for CloudFront"
+  description = "Project name used for tagging/naming"
   type        = string
 }
 
 variable "aws_region" {
-  description = "AWS region"
+  description = "AWS region for all regional services"
   type        = string
-  default     = "us-east-1"
+  default     = "ap-south-1"
+}
+
+variable "bucket_name" {
+  description = "Globally unique S3 bucket name for the site"
+  type        = string
 }
 
 variable "alarm_email" {
-  description = "Email for SNS alarms"
+  description = "Optional email address to receive CloudWatch alarm notifications"
   type        = string
   default     = ""
 }
