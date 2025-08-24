58cc876d2c5c41bd5cb21cd12d9393225e287b2f
Chaitali Charandas Daware <126700530+chaitali-daware@users.noreply.github.com>
2025-08-24 09:28:10 +0530

    outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -3,5 +3,13 @@ output "s3_bucket_name" {
 }
 
 output "s3_website_endpoint" {
-  value = aws_s3_bucket_website_configuration.website_config.website_endpoint
+  value = aws_s3_bucket.website.website_endpoint
+}
+
+output "cloudfront_url" {
+  value = aws_cloudfront_distribution.cdn.domain_name
+}
+
+output "acm_cname_validation" {
+  value = [for o in aws_acm_certificate.cert.domain_validation_options : o.resource_record_name + " -> " + o.resource_record_value]
 }
