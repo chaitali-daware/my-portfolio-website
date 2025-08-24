9a5af7ef0404976e480db11a3be64e4068301fc4
Chaitali Charandas Daware <126700530+chaitali-daware@users.noreply.github.com>
2025-08-24 10:31:23 +0530

    main.tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -2,9 +2,9 @@ provider "aws" {
   region = var.aws_region
 }
 
-###########################
-# S3 Bucket for Website
-###########################
+# ----------------------------
+# S3 Bucket
+# ----------------------------
 resource "aws_s3_bucket" "website" {
   bucket = var.bucket_name
 }
@@ -19,15 +19,14 @@ resource "aws_s3_bucket_public_access_block" "block" {
 
 resource "aws_s3_bucket_policy" "public_read" {
   bucket = aws_s3_bucket.website.id
-
   policy = jsonencode({
-    Version = "2012-10-17"
+    Version = "2012-10-17",
     Statement = [
       {
-        Effect    = "Allow"
-        Principal = "*"
-        Action    = "s3:GetObject"
-        Resource  = "${aws_s3_bucket.website.arn}/*"
+        Effect = "Allow",
+        Principal = "*",
+        Action = "s3:GetObject",
+        Resource = "${aws_s3_bucket.website.arn}/*"
       }
     ]
   })
@@ -36,44 +35,57 @@ resource "aws_s3_bucket_policy" "public_read" {
 resource "aws_s3_bucket_website_configuration" "website_config" {
   bucket = aws_s3_bucket.website.id
 
-  index_document { suffix = "index.html" }
-  error_document { key = "index.html" }
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "index.html"
+  }
 }
 
-# Upload all frontend files
+# ----------------------------
+# Upload frontend files
+# ----------------------------
 resource "aws_s3_object" "website_files" {
-  for_each = { for file in fileset(var.frontend_folder, "**/*") : file => file }
+  for_each = { for file in fileset(var.frontend_dir, "**/*") : file => file }
 
   bucket = aws_s3_bucket.website.id
   key    = each.value
-  source = "${var.frontend_folder}/${each.value}"
+  source = "${var.frontend_dir}/${each.value}"
 }
-# ACM certificate (DNS validation)
+
+# ----------------------------
+# ACM Certificate (DNS validation)
+# ----------------------------
 resource "aws_acm_certificate" "cert" {
   domain_name       = var.domain_name
   validation_method = "DNS"
 }
 
-
-
-resource "aws_acm_certificate_validation" "cert_validation" {
-  certificate_arn         = aws_acm_certificate.cert.arn
-  validation_record_fqdns = [for record in aws_route53_record.acm_validation : record.fqdn]
-}
+# ----------------------------
+# CloudFront Distribution
+# ----------------------------
 resource "aws_cloudfront_distribution" "cdn" {
-  origin {
-    domain_name = aws_s3_bucket.website.bucket_regional_domain_name
-    origin_id   = "S3-${aws_s3_bucket.website.id}"
-  }
-
   enabled             = true
-  is_ipv6_enabled     = true
   default_root_object = "index.html"
+
+  origins {
+    domain_name = aws_s3_bucket_website_configuration.website_config.website_endpoint
+    origin_id   = "s3-website-${aws_s3_bucket.website.id}"
+
+    custom_origin_config {
+      http_port              = 80
+      https_port             = 443
+      origin_protocol_policy = "http-only"
+      origin_ssl_protocols   = ["TLSv1.2"]
+    }
+  }
 
   default_cache_behavior {
     allowed_methods  = ["GET", "HEAD", "OPTIONS"]
     cached_methods   = ["GET", "HEAD"]
-    target_origin_id = "S3-${aws_s3_bucket.website.id}"
+    target_origin_id = "s3-website-${aws_s3_bucket.website.id}"
 
     forwarded_values {
       query_string = false
@@ -85,67 +97,17 @@ resource "aws_cloudfront_distribution" "cdn" {
     viewer_protocol_policy = "redirect-to-https"
   }
 
-  restrictions {
-    geo_restriction {
-      restriction_type = "none"
-    }
-  }
-
   viewer_certificate {
     acm_certificate_arn            = aws_acm_certificate.cert.arn
     ssl_support_method             = "sni-only"
     minimum_protocol_version       = "TLSv1.2_2021"
   }
 
+  restrictions {
+    geo_restriction {
+      restriction_type = "none"
+    }
+  }
+
   aliases = [var.domain_name]
 }
-resource "aws_iam_role" "lambda_role" {
-  name = "lambda_execution_role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [{
-      Action    = "sts:AssumeRole"
-      Effect    = "Allow"
-      Principal = { Service = "lambda.amazonaws.com" }
-    }]
-  })
-}
-
-resource "aws_iam_role_policy_attachment" "lambda_policy_attach" {
-  role       = aws_iam_role.lambda_role.name
-  policy_arn = "arn:aws:iam::aws:policy/AWSLambdaBasicExecutionRole"
-}
-
-resource "aws_lambda_function" "handle_contact" {
-  filename         = "${var.lambda_folder}/function.zip"
-  function_name    = "handleContactForm"
-  handler          = "handleContactForm.lambda_handler"
-  runtime          = "python3.11"
-  role             = aws_iam_role.lambda_role.arn
-  source_code_hash = filebase64sha256("${var.lambda_folder}/function.zip")
-}
-
-resource "aws_lambda_function" "log_visitor" {
-  filename         = "${var.lambda_folder}/visitor.zip"
-  function_name    = "logVisitorData"
-  handler          = "logVisitorData.lambda_handler"
-  runtime          = "python3.11"
-  role             = aws_iam_role.lambda_role.arn
-  source_code_hash = filebase64sha256("${var.lambda_folder}/visitor.zip")
-}
-# CloudWatch Alarms for Lambda errors
-resource "aws_cloudwatch_metric_alarm" "lambda_errors" {
-  alarm_name          = "LambdaErrorAlarm"
-  comparison_operator = "GreaterThanThreshold"
-  evaluation_periods  = 1
-  metric_name         = "Errors"
-  namespace           = "AWS/Lambda"
-  period              = 60
-  statistic           = "Sum"
-  threshold           = 0
-
-  dimensions = {
-    FunctionName = aws_lambda_function.handle_contact.function_name
-  }
-}
-
