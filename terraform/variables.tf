57cb42614b338a34a38fa7415bec20630559e571
Chaitali Charandas Daware <126700530+chaitali-daware@users.noreply.github.com>
2025-08-25 02:34:24 +0530

    variables.tf

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -1,33 +1,17 @@
-terraform {
-  required_version = ">= 1.6.0"
-  required_providers {
-    aws     = { source = "hashicorp/aws", version = ">= 5.30" }
-    archive = { source = "hashicorp/archive", version = ">= 2.4.0" }
-  }
-}
-
-provider "aws" {
-  region = var.aws_region
-}
-
-variable "project" {
-  description = "Project name used for tagging/naming"
-  type        = string
-}
-
-variable "aws_region" {
-  description = "AWS region for all regional services"
-  type        = string
-  default     = "ap-south-1"
-}
-
 variable "bucket_name" {
-  description = "Globally unique S3 bucket name for the site"
+  description = "Name of the S3 bucket used to host the portfolio website"
   type        = string
+  default     = "chaitalidaware.me"
 }
 
-variable "alarm_email" {
-  description = "Optional email address to receive CloudWatch alarm notifications"
+variable "domain_name" {
+  description = "Your custom domain name used with CloudFront and HTTPS"
   type        = string
-  default     = ""
+  default     = "chaitalidaware.me"
+}
+
+variable "acm_certificate_arn" {
+  description = "ACM certificate ARN for enabling HTTPS with CloudFront (must be in us-east-1)"
+  type        = string
+  default     = "arn:aws:acm:us-east-1:058264155367:certificate/a43ec253-95bb-4ea2-bdb3-362cb6154244"
 }
