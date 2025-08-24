aaee8aa3235b48c93b597cc1e73e15b9f6ddcd3b
Chaitali Charandas Daware <126700530+chaitali-daware@users.noreply.github.com>
2025-08-24 08:14:30 +0530

    outputs.tf

--- terraform/outputs.tf
+++ terraform/outputs.tf
@@ -1,9 +1,7 @@
 output "s3_bucket_name" {
-  description = "The name of the S3 bucket"
-  value       = aws_s3_bucket.website.id
+  value = aws_s3_bucket.website.id
 }
 
 output "s3_website_endpoint" {
-  description = "The S3 website endpoint URL"
-  value       = aws_s3_bucket.website.website_endpoint
+  value = aws_s3_bucket.website.bucket_regional_domain_name
 }
